358e2c08bdb137f5c8a33287461c00cbdf6d2624
super1-1star <agsuperstar1142@gmail.com>
2021-12-31 02:41:57 +0530

    84

--- server.js
+++ server.js
@@ -4,20 +4,37 @@ const app = require('./app');
 
 dotenv.config({ path: './config.env' });
 
-
 const DB = process.env.DATABASE.replace(
   '<PASSWORD>',
   process.env.DATABASE_PASSWORD
 );
 
-mongoose.connect(DB,{
-  useNewUrlParser:true,
-  useCreateIndex: true,
-  useFindAndModify: false
-}).then(cl=>{
-  console.log(cl.connections);
-  console.log('DB connections successfull');
-})
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  })
+  .then(() => console.log('DB server connect'))
+  .catch((e) => console.log('DB error', e));
+
+const tourSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'A tour must have a name'],
+    unique: true,
+  },
+  rating: {
+    type: Number,
+  default: 4.5,
+  },
+  price: {
+    type: Number,
+    required: [true, 'A tour have a price'],
+  },
+});
+
+const Tour = mongoose.model('Tour', tourSchema);
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
