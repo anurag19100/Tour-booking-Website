97535d1ae15855bb34190564e86e09fc563c2e1d
super1-1star <agsuperstar1142@gmail.com>
2021-12-31 01:22:37 +0530

    update local db

--- server.js
+++ server.js
@@ -1,7 +1,23 @@
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const app = require('./app');
 
 dotenv.config({ path: './config.env' });
+
+
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD
+);
+
+mongoose.connect(DB,{
+  useNewUrlParser:true,
+  useCreateIndex: true,
+  useFindAndModify: false
+}).then(cl=>{
+  console.log(cl.connections);
+  console.log('DB connections successfull');
+})
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
