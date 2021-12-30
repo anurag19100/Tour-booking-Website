d333064bfa17e4e7aec4ec0a8bd7068f1886f67a
super1-1star <agsuperstar1142@gmail.com>
2021-12-31 03:28:32 +0530

    rearrange in monodb

--- server.js
+++ server.js
@@ -18,23 +18,20 @@ mongoose
   .then(() => console.log('DB server connect'))
   .catch((e) => console.log('DB error', e));
 
-const tourSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: [true, 'A tour must have a name'],
-    unique: true,
-  },
-  rating: {
-    type: Number,
-  default: 4.5,
-  },
-  price: {
-    type: Number,
-    required: [true, 'A tour have a price'],
-  },
-});
+// const testTour = new Tour({
+//   name: 'superstar1',
+//   rating: 4,
+//   price: 450,
+// });
 
-const Tour = mongoose.model('Tour', tourSchema);
+// testTour
+//   .save()
+//   .then((doc) => {
+//     console.log(doc);
+//   })
+//   .catch((err) => {
+//     console.log(err);
+//   });
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
